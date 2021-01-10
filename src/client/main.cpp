d1ce2eb1aa4b3f8b1c961af72d5877782e982629
Paul Storch <sto.paul14@gmail.com>
2021-01-10 16:05:14 +0100

    Adding check function and bug fixes

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <future>
 #include <bitset>
-#include <boost/dynamic_bitset/dynamic_bitset.hpp>
+#include <vector>
 
 #include "asio_include_dir/asio.hpp"
 #include "cli11_include_dir/CLI11.hpp"
@@ -10,119 +10,67 @@
 using namespace std;
 using namespace asio::ip;
 
-string check_callable(const string& message){
-    string checklist{" !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"};
-    size_t check{message.find_first_not_of(checklist)};
-    if(check == string::npos){
-        return "";
-    }
-    else{
-        return string(message) + " contains not ASCII character";
-    }  
+bitset<80> getStream(tcp::iostream& strm){
+    bitset<80> data;
+    strm >> data;
+    //cout << data.to_string(); << endl;
+    return data;
 }
 
-vector<bitset<7>> convertAsciiToBinary(string asciis){
-    vector<bitset<7>> asciivector{};
-    for(char ascii: asciis){
-        asciivector.push_back(ascii);
-    }
-    return asciivector;
-}
-
-vector<bitset<8>> onedParitaet(vector<bitset<7>> asciibits){
-    vector<bitset<8>> onedParitaet{};
-    for(auto bits: asciibits){
-        int countOnes{0};
-        for(auto bit: bits.to_string()){
-            if(bit == '1'){
-                countOnes++;
-            }
-        }
-        if(countOnes % 2 == 1){
-            bitset<8> temp(bits.to_string() + bitset<1>(0x1).to_string());
-            onedParitaet.push_back(temp);
-        }else {
-            bitset<8> temp(bits.to_string() + bitset<1>(0x0).to_string());
-            onedParitaet.push_back(temp);
-        }
-    }
-    return onedParitaet;
-}
-
-vector<bitset<80>> twodParitaet(vector<bitset<8>> onedParitaet){
-    vector<bitset<80>> block{};
-    vector<int> countOnes{0,0,0,0,0,0,0,0};
-    string blockstring{};
-    int counter{0};
-    for(auto bits: onedParitaet){
-        counter++;
-        for(int i = 0; i < 8; i++){
-            if(bits.to_string().at(i) == '1'){
-                countOnes.at(i)++;
-            }
-        }
-        blockstring += bits.to_string();
-        if(counter % 9 == 0){
-            for(auto ones: countOnes){
-                if(ones % 2 == 1){
-                    blockstring += bitset<1>(0x1).to_string();
-                } else{
-                    blockstring += bitset<1>(0x0).to_string();
+bool checkStream(vector<shared_future<bitset<80>>> message){
+    for(auto blocks : message){
+        vector<int> countOnes{0,0,0,0,0,0,0,0};
+        int counter{0};
+        int onedcounter{0};
+        for(auto bit : blocks.get().to_string()){
+            if(counter % 8 == 0 && counter <= 72){
+                if(onedcounter % 2 == 1){
+                    return false;
                 }
+                onedcounter = 0;
             }
-            block.push_back(bitset<80>(blockstring));
-            blockstring = "";
+            if(bit == '1'){
+                countOnes.at(counter % 8)++;
+                onedcounter++;
+            }
+        counter++;
         }
-    }
-    if(blockstring.size() != 0){
-        while(blockstring.size() != 72){
-            blockstring += bitset<1>(0x0).to_string();
-        }
-        for(auto ones: countOnes){
+        for(auto ones : countOnes){
             if(ones % 2 == 1){
-                blockstring += bitset<1>(0x1).to_string();
-            } else{
-                blockstring += bitset<1>(0x0).to_string();
+                return false;
             }
         }
-        block.push_back(bitset<80>(blockstring));
     }
-    return block;
-}
-
-vector<bitset<80>> addParitaetToBits(vector<bitset<7>> asciibits){
-    vector<bitset<80>> blocks{twodParitaet(onedParitaet(asciibits))};
-    return blocks;
+    return true;
 }
 
 int main(int argc, char *argv[]) {
     CLI::App app("Server for ASCII-Code transfer");
     string ipaddress{"localhost"};
     string port{"8888"};
-    string ascii{};
-    app.add_option("ASCII", ascii, "Message in ASCII character")->required()->check(check_callable);
     app.add_option("-i,--ipv4address", ipaddress, "IP-Adress to connect!");
     app.add_option("-p,--port", port, "Port to connect!");
     CLI11_PARSE(app, argc, argv);
 
-    vector<bitset<7>> asciibits{convertAsciiToBinary(ascii)};
-    vector<bitset<80>> blocks{addParitaetToBits(asciibits)};
-
     tcp::iostream strm{ipaddress, port};
     strm.expires_after(10s);
-    if (strm){
-        strm << ascii << endl;
-        /*
-        string data;
-        getline(strm, data);
-        if (strm){
-            cout << data << endl;
-        } else {
-            cout << strm.error().message() << endl;
-        } */
-        strm.close();
-    }
+    
 
+    vector<shared_future<bitset<80>>> message{};
+    std::future_status status;
+    int counter{0};
+    while(true){ 
+        message.push_back(async(launch::async, getStream, ref(strm)));
+        status = message.at(counter).wait_for(std::chrono::seconds(2));
+        if(status == std::future_status::ready){
+            cout << message.at(counter).get().to_string() << endl;
+            counter++;
+        } else {
+            break;
+        }
+    }
+    strm.close();
+    cout << checkStream(message) << endl;
 
     return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <future>
 #include <string>
+#include <vector>
+#include <bitset>
 
 #include "asio_include_dir/asio.hpp"
 #include "cli11_include_dir/CLI11.hpp"
@@ -10,21 +12,134 @@
 using namespace std;
 using namespace asio::ip;
 
-string getStream(tcp::socket& sock){
-    tcp::iostream strm{std::move(sock)};
-    string data;
-    strm >> data;
-    //cout << data << endl;
-    strm.close();
-    return data;
+
+
+string check_callable(const string& message){
+    string checklist{" !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"};
+    size_t check{message.find_first_not_of(checklist)};
+    if(check == string::npos){
+        return "";
+    }
+    else{
+        return string(message) + " contains not ASCII character";
+    }  
 }
 
+vector<bitset<7>> convertAsciiToBinary(string asciis){
+    vector<bitset<7>> asciivector{};
+    for(char ascii: asciis){
+        asciivector.push_back(ascii);
+    }
+    return asciivector;
+}
+
+vector<bitset<8>> onedParitaet(vector<bitset<7>> asciibits){
+    vector<bitset<8>> onedParitaet{};
+    for(auto bits: asciibits){
+        int countOnes{0};
+        for(auto bit: bits.to_string()){
+            if(bit == '1'){
+                countOnes++;
+            }
+        }
+        if(countOnes % 2 == 1){
+            bitset<8> temp(bits.to_string() + bitset<1>(0x1).to_string());
+            onedParitaet.push_back(temp);
+        }else {
+            bitset<8> temp(bits.to_string() + bitset<1>(0x0).to_string());
+            onedParitaet.push_back(temp);
+        }
+    }
+    return onedParitaet;
+}
+
+vector<bitset<80>> twodParitaet(vector<bitset<8>> onedParitaet){
+    vector<bitset<80>> block{};
+    vector<int> countOnes{0,0,0,0,0,0,0,0};
+    string blockstring{};
+    int counter{0};
+    for(auto bits: onedParitaet){
+        counter++;
+        for(int i = 0; i < 8; i++){
+            if(bits.to_string().at(i) == '1'){
+                countOnes.at(i)++;
+            }
+        }
+        blockstring += bits.to_string();
+        if(counter % 9 == 0){
+            for(int i = 0; i < 8; i++){
+                if(countOnes.at(i) % 2 == 1){
+                    blockstring += bitset<1>(0x1).to_string();
+                } else{
+                    blockstring += bitset<1>(0x0).to_string();
+                }
+            countOnes.at(i) = 0;
+            }
+            block.push_back(bitset<80>(blockstring));
+            blockstring = "";
+        }
+    }
+    if(blockstring.size() != 0){
+        while(blockstring.size() != 72){
+            blockstring += bitset<1>(0x0).to_string();
+        }
+        for(auto ones : countOnes){
+            if(ones % 2 == 1){
+                blockstring += bitset<1>(0x1).to_string();
+            } else{
+                blockstring += bitset<1>(0x0).to_string();
+            }
+        }
+        block.push_back(bitset<80>(blockstring));
+    }
+    return block;
+}
+
+vector<bitset<80>> addParitaetToBits(vector<bitset<7>> asciibits){
+    vector<bitset<80>> blocks{twodParitaet(onedParitaet(asciibits))};
+    return blocks;
+}
+
+bool checkBlocks(vector<bitset<80>> blocks){
+    for(auto block : blocks){
+        vector<int> countOnes{0,0,0,0,0,0,0,0};
+        int counter{0};
+        int onedcounter{0};
+        for(auto bit : block.to_string()){
+            if(counter % 8 == 0 && counter <= 72){
+                if(onedcounter % 2 == 1){
+                    return false;
+                }
+                onedcounter = 0;
+            }
+            if(bit == '1'){
+                countOnes.at(counter % 8)++;
+                onedcounter++;
+            }
+        counter++;
+        }
+        for(auto ones : countOnes){
+            if(ones % 2 == 1){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 int main(int argc, char *argv[]) {
     CLI::App app("Client for ASCII-Code transfer");
     u_short port{8888};
+    string ascii{};
+    app.add_option("ASCII", ascii, "Message in ASCII character")->required()->check(check_callable);
     app.add_option("-p,--port", port, "Port for hosting!");
     CLI11_PARSE(app, argc, argv);
+
+    vector<bitset<7>> asciibits{convertAsciiToBinary(ascii)};
+    vector<bitset<80>> blocks{addParitaetToBits(asciibits)};
+    if(!checkBlcoks(blocks)){
+        cerr << "ERROR-Detection Failed!!" << endl;
+    }
 
     asio::io_context ctx;
     tcp::endpoint ep{tcp::v4(), 8888};
@@ -33,10 +148,16 @@ int main(int argc, char *argv[]) {
 
     tcp::socket sock{ctx};
     acceptor.accept(sock);
+    tcp::iostream strm{std::move(sock)};
 
-    shared_future<string> message{async(launch::async, getStream, ref(sock))};
-    cout << message.get() << endl;
-
+    if (strm){
+        for(auto block : blocks){
+            strm << block;
+            this_thread::sleep_for(1s);
+        }
+        this_thread::sleep_for(4s);
+        strm.close();
+    }
 
     return 0;
 }
