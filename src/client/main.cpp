999efef2785f5b929a8b7d2337a44e2a96cd97f6
Paul Storch <sto.paul14@gmail.com>
2021-01-10 14:23:01 +0100

    deleting .vscode

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -29,6 +29,71 @@ vector<bitset<7>> convertAsciiToBinary(string asciis){
     return asciivector;
 }
 
+vector<bitset<8>> onedParitaet(vector<bitset<7>> asciibits){
+    vector<bitset<8>> onedParitaet{};
+    for(auto bits: asciibits){
+        int countOnes{0};
+        for(auto bit: bits.to_string()){
+            if(bit == '1'){
+                countOnes++;
+            }
+        }
+        if(countOnes % 2 == 1){
+            bitset<8> temp(bits.to_string() + bitset<1>(0x1).to_string());
+            onedParitaet.push_back(temp);
+        }else {
+            bitset<8> temp(bits.to_string() + bitset<1>(0x0).to_string());
+            onedParitaet.push_back(temp);
+        }
+    }
+    return onedParitaet;
+}
+
+vector<bitset<80>> twodParitaet(vector<bitset<8>> onedParitaet){
+    vector<bitset<80>> block{};
+    vector<int> countOnes{0,0,0,0,0,0,0,0};
+    string blockstring{};
+    int counter{0};
+    for(auto bits: onedParitaet){
+        counter++;
+        for(int i = 0; i < 8; i++){
+            if(bits.to_string().at(i) == '1'){
+                countOnes.at(i)++;
+            }
+        }
+        blockstring += bits.to_string();
+        if(counter % 9 == 0){
+            for(auto ones: countOnes){
+                if(ones % 2 == 1){
+                    blockstring += bitset<1>(0x1).to_string();
+                } else{
+                    blockstring += bitset<1>(0x0).to_string();
+                }
+            }
+            block.push_back(bitset<80>(blockstring));
+            blockstring = "";
+        }
+    }
+    if(blockstring.size() != 0){
+        while(blockstring.size() != 72){
+            blockstring += bitset<1>(0x0).to_string();
+        }
+        for(auto ones: countOnes){
+            if(ones % 2 == 1){
+                blockstring += bitset<1>(0x1).to_string();
+            } else{
+                blockstring += bitset<1>(0x0).to_string();
+            }
+        }
+        block.push_back(bitset<80>(blockstring));
+    }
+    return block;
+}
+
+vector<bitset<80>> addParitaetToBits(vector<bitset<7>> asciibits){
+    vector<bitset<80>> blocks{twodParitaet(onedParitaet(asciibits))};
+    return blocks;
+}
 
 int main(int argc, char *argv[]) {
     CLI::App app("Server for ASCII-Code transfer");
@@ -41,6 +106,7 @@ int main(int argc, char *argv[]) {
     CLI11_PARSE(app, argc, argv);
 
     vector<bitset<7>> asciibits{convertAsciiToBinary(ascii)};
+    vector<bitset<80>> blocks{addParitaetToBits(asciibits)};
 
     tcp::iostream strm{ipaddress, port};
     strm.expires_after(10s);
